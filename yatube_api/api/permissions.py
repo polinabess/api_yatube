0c41cc2ca301ce67f798e2e20614bb3a41240bc8
Polina Bessogonova <girls_kiss@mail.ru>
2023-01-17 19:28:54 +0300

    fixed warnings of PEP8

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,4 +5,3 @@ class AuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
-
